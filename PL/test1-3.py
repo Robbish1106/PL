6806551f6087008145f808f1e65c975522c2fad7
BRUHman <109062504+Robbish1106@users.noreply.github.com>
2022-09-22 11:02:11 +0800

    Add files via upload

---
+++ PL/test1-3.py
@@ -0,0 +1,10 @@
+event=input("請輸入案件:")
+
+if event =="被告闖紅燈":
+    print("被告賠100%")
+elif event =="原告闖紅燈":
+    print("原告多被告少")
+elif event=="被告原告皆闖紅燈":
+    print("原告50%被告50%")
+else:
+    print("請填入有效訊息")
